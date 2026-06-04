bf3c2fe703ec417ddc96ed59be988813e1ad8862
snktcodes <92163101+snktcodes@users.noreply.github.com>
2026-06-04 09:35:34 +0530

    commit

---
+++ finalbuildfiles/2026/May/adform/dji/loadertagA.js
@@ -0,0 +1,3 @@
+document.write('<iframe src="https://creativehubiion.github.io/advertiser-creatives/finalbuildfiles/2026/adform/dji/index.html?clickTAG=%%c1;cpdir=https://www.werkenbijdji.nl" ' +
+    'width="320px" height="480px" frameborder="0" scrolling="no" ' +
+    'allow="autoplay; fullscreen; encrypted-media;"></iframe>');
